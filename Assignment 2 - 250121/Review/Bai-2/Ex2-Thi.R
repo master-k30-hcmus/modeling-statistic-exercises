7112c16ddf47ead1857e9446b03e460a1f8b24d3
Thi Dang <thidang@kms-technology.com>
2021-01-28 12:04:59 +0700

    Thi: update ex-2-3

--- Assignment 2 - 250121/Review/Bai-2/Ex2-Thi.R
+++ Assignment 2 - 250121/Review/Bai-2/Ex2-Thi.R
@@ -6,6 +6,11 @@ Y = c(37.8, 22.5, 17.1, 10.8, 7.2, 42.3, 30.2, 19.4, 14.8, 9.5, 32.4, 21.6)
 X1 = c(4, 4, 3, 2, 1, 6, 4, 4, 1, 1, 3, 4)
 X2 = c(4.0, 3.6, 3.1, 3.2, 3.0, 3.8, 3.8, 2.9, 3.8, 2.8, 3.4, 2.8)
 
+## Cau 3: Bang ANOVA cua cho mo hinh hoi quy 2 bien la
+
+lm_fit = lm(Y ~ X1 + X2)
+anova(lm_fit)
+
 ## Cau 4: Kiem dinh cho gia thiet H0: B1=B2=0 voi muc y nghia alpha=0.05
 
 n = length(Y)
