71aaa89b82f781ec33c30baccd4c9baa13b96fd3
lyphilong <lyphilong7969@gmail.com>
2021-01-29 13:56:34 +0700

    merge Ex3

--- Assignment 2 - 250121/Review/Bai-3/Ex3-merged.R
+++ Assignment 2 - 250121/Review/Bai-3/Ex3-merged.R
@@ -1,3 +1,4 @@
+## Tao bo du lieu
 X1 <- c(2,1,3,6,7,8,8,5,5,8,4,9,12,7)
 X2 <- c(45,43,43,47,42,41,32,33,41,38,32,31,35,29)
 X3 <- c(121,132,154,145,129,156,132,147,128,163,161,172,174,180)
@@ -6,13 +7,13 @@ Y <- c(12,14,10,16,14,19,21,19,21,16,19,21,25,21)
 n <- length(Y)
 p <- 3
 
-##1
+## 1
 ##Xem thu cac bien co doc lap tuyen tinh
 library("PerformanceAnalytics")
 data <- data.frame(X1, X2, X3)
 chart.Correlation(data, histogram=TRUE, pch="+")
 
-
+## Cac mo hinh tuyen tinh voi 2 bien doc lap 
 linearMod3 <- lm(Y ~ X1 + X2)
 summary(linearMod3)$coefficient
 linearMod2 <- lm(Y ~ X1 + X3)
@@ -34,7 +35,7 @@ if (max(Rsquare_1,Rsquare_2,Rsquare_3) == Rsquare_3){
 } else if(max(Rsquare_1,Rsquare_2,Rsquare_3) == Rsquare_2){
   print("Mo hinh voi 2 bien doc lap X1 va X3 la tot nhat")
 } else {
-  print("Mo hinh voi 2 bien doc lap X2 va X3 tot nhat") 
+  print("Mo hinh voi 2 bien doc lap X2 va X3 tot nhat")
 }
 
 ## 7
@@ -55,5 +56,7 @@ var_beta = var_epsilon*solve(t(X)%*%X)
 ## 10
 linearMod_X1 <- lm(Y ~ X1)
 if(summary(linearMod_X1)$adj.r.squared < summary(linearModFull)$adj.r.squared){
-  print("Mo hinh khi them bien x2 va x3 tot hon x1")
+  print("Khi them 2 bien doc lap X3 và X2 vao mo hinh thi chat luong mo hinh tot hon")
+} else {
+  print("Khi them 2 bien doc lap X3 va X2 vao mo hinh thi chat luong khong tot hon")
 }
