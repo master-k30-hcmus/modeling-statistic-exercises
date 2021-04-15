2cd89099e8c895fb69f424f15796badea3ce9d11
Thi Dang <thidang@kms-technology.com>
2021-04-15 16:20:10 +0700

    update b3

--- Final - 120421/data3-Thi.R
+++ Final - 120421/data3-Thi.R
@@ -30,42 +30,28 @@ aggregate(y_i ~ x_i.4,data3,mean)
 aggregate(y_i ~ x_i.10,data3,mean)
 
 
-# ## Determining the association between variables
-# cor_data=data.frame(data3)
-# correlation=cor(cor_data)
-# par(mfrow=c(1, 1))
-# corrplot(correlation,method="color")
+## Determining the association between variables
+cor_data=data.frame(data3)
+correlation=cor(cor_data)
+par(mfrow=c(1, 1))
+corrplot(correlation,method="color")
 
-# ## Create full model
-# mod_full_1 = lm(price ~ ., data3) #full model
-# summary(mod_full_1)
-# coef(mod_full_1)
-# par(mfrow=c(2,2))
-# plot(mod_full_1)
-#
-# ## model selection with BIC criteria, stepwise backward
-# mod_BIC_1 <- MASS::stepAIC(mod_full_1, direction = "backward", k = log(nrow(data3)))
-# summary(mod_BIC_1)
-# mod_BIC_1$anova
-# par(mfrow=c(2,2))
-# plot(mod_BIC_1)
-#
-# ## transform PRICE
-# mod_full_2 = lm(log(price) ~ . , data3)
-# summary(mod_full_2)
-# par(mfrow=c(2,2))
-# plot(mod_full_2)
-#
-# ## transform PRICE and remove BASEMENT AREA
-# data3_new <- subset(mydata, select = -c(id,date,sqft_basement)) #remove sqft_basement
-# mod_full_3 = lm(log(price) ~ . , data3_new)
-# summary(mod_full_3)
-# coef(mod_full_3)
-# par(mfrow=c(2,2))
-# plot(mod_full_3)
-#
-# #model selection with BIC criteria, stepwise backward
-# mod_BIC_2 <- MASS::stepAIC(mod_full_3, direction = "backward", k = log(nrow(data3_new)))
-# summary(mod_BIC_2)
-# mmps(mod_BIC_2)
-# avPlots(mod_BIC_2)
+## Create full model
+mod_full_1 = lm(y_i ~ ., data3) #full model
+summary(mod_full_1)
+coef(mod_full_1)
+par(mfrow=c(2,2))
+plot(mod_full_1)
+vif(mod_full_1)
+
+## model selection with BIC criteria, stepwise
+mod_BIC_1 <- MASS::stepAIC(mod_full_1, direction = "both", k = log(nrow(data3)))
+summary(mod_BIC_1)
+mod_BIC_1$anova
+par(mfrow=c(2,2))
+plot(mod_BIC_1)
+
+vif(mod_BIC_1)
+mmps(mod_BIC_1)
+avPlots(mod_BIC_1)
+coef(mod_BIC_1)
